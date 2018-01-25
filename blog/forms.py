194a5f4e9fa545a171f6560479650020471515ad
cindy-yin <cindy.xin.yin@gmail.com>
2018-01-25 21:19:48 +0000

    djangogirls extension

--- blog/forms.py
+++ blog/forms.py
@@ -1,10 +1,16 @@
 # define own form PostForm according to Post data model,specify which field should be displayed for the form
 
 from django import forms
-from .models import Post
+from .models import Post,Comment
 
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
         fields = ('title','text')
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('author', 'text')
--- mysite/settings.py
+++ mysite/settings.py
@@ -120,3 +120,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+#login-success redirect
+LOGIN_REDIRECT_URL = '/'
